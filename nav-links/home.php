41e4203f6af3b6e04af3493961f72b7553009ed4
Naman Dutta <37544286+duttanaman1@users.noreply.github.com>
2020-05-30 20:07:51 +0530

    Add files via upload

---
+++ nav-links/home.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="description" content="PONIGYM Template" />
+  <meta name="keywords" content="PONIGYM, unica, creative, html" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title></title>
+  <script src="js/typewriter.js"></script>
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap" rel="stylesheet" />
+  <link href="https://fonts.googleapis.com/css?family=Oswald:300,400,500,600,700&display=swap" rel="stylesheet" />
+
+  <!-- Css Styles -->
+  <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
+  <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css" />
+  <link rel="stylesheet" href="css/themify-icons.css" type="text/css" />
+  <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css" />
+  <link rel="stylesheet" href="css/magnific-popup.css" type="text/css" />
+  <link rel="stylesheet" href="css/slicknav.min.css" type="text/css" />
+  <link rel="stylesheet" href="css/style.css" type="text/css" />
+</head>
+
+<body>
+  <!-- Page Preloder -->
+  <div id="preloder">
+    <div class="loader"></div>
+  </div>
+
+  <!-- Header Section Begin -->
+  <header class="header-section header-normal">
+    <div class="container">
+      <div class="logo">
+        <a href="../index.html">
+          LOGO
+        </a>
+      </div>
+      <div class="nav-menu">
+        <nav class="mainmenu mobile-menu">
+          <ul>
+            <li><a href="../index.php">Login</a></li>
+          </ul>
+
+        </nav>
+        <div class="nav-right search-switch">
+          <i class="ti-search"></i>
+        </div>
+      </div>
+      <div id="mobile-menu-wrap"></div>
+    </div>
+  </header>
+  <!-- Header End -->
+
+  <!-- Map Section Begin -->
+  <div class="contact-map">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d33245.297803635964!2d-73.76987401620775!3d40.704774398815005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sbd!4v1575866843291!5m2!1sen!2sbd" style="border: 0;" allowfullscreen="" alt="Map Display"></iframe>
+    <i class="fa fa-map-marker"></i>
+  </div>
+  <!-- Map Section End -->
+
+  <!-- About Us Secion Begin -->
+  <section class="about-us-section spad">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5">
+          <div class="about-pic">
+            <img src="img/about-us.jpg" alt="" />
+          </div>
+        </div>
+        <div class="col-lg-6 offset-lg-1">
+          <div class="about-text">
+            <div class="section-title">
+              <h2> <a href="" class="typewrite" data-period="2000" data-type="[ &quot;Welcome to LOGO&quot;, &quot;You can buy here anything &quot;, &quot;Shopping is Fun&quot;, &quot;&quot; ]"><span class="wrap"></span></a>
+              </h2>
+              <p>
+                Our dedicated Ponigym areas and fitness experts can help you
+                discover new training techniques and exercises that offer a
+                dynamic and efficient full-body workout.
+              </p>
+            </div>
+            <p>
+              Our fitness experts can help you discover new training
+              techniques. Lorem ipsum dolor sit amet, consectetur adipisicing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+              ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+              aute irure dolor in reprehenderit in voluptate velit esse cillum
+              dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+              cupidatat non proident, sunt in culpa qui officia deserunt
+              mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- About Us Secion End -->
+  <!-- About Page Trainer Section Begin -->
+  <section class="about-page-trainer spad">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="col-lg-12">
+            <div class="section-title">
+              <h2>Our Shops</h2>
+              <p>
+                Our fitness experts can help you discover new training
+                techniques.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-sm-6">
+          <div class="single-trainer-item">
+            <div class="trainer-pic">
+              <img src="img/trainer/trainer-1.jpg" alt="" />
+            </div>
+            <div class="trainer-text">
+              <h5>Shop1</h5>
+              <span>Address1</span>
+              <div class="trainer-social">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="single-trainer-item">
+            <div class="trainer-pic">
+              <img src="img/trainer/trainer-2.jpg" alt="" />
+            </div>
+            <div class="trainer-text">
+              <h5>Shop2</h5>
+              <span>Address2</span>
+              <div class="trainer-social">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="single-trainer-item">
+            <div class="trainer-pic">
+              <img src="img/trainer/trainer-3.jpg" alt="" />
+            </div>
+            <div class="trainer-text">
+              <h5>Shop3</h5>
+              <span>Address3</span>
+              <div class="trainer-social">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="single-trainer-item">
+            <div class="trainer-pic">
+              <img src="img/trainer/trainer-4.jpg" alt="" />
+            </div>
+            <div class="trainer-text">
+              <h5>Shop4</h5>
+              <span>Address4</span>
+              <div class="trainer-social">
+                <a href="#"><i class="fa fa-facebook"></i></a>
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-instagram"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- About Page Trainer Section End -->
+
+
+  <!-- Footer Section Begin -->
+  <footer class="footer-section">
+    <div class="register normal-register">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="copyright">
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              Copyright &copy;
+              <script>
+                document.write(new Date().getFullYear());
+              </script>
+              All rights reserved
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </div>
+
+            <div class="footer-links">
+              <a href="#"><i class="fa fa-facebook"></i></a>
+              <a href="#"><i class="fa fa-twitter"></i></a>
+              <a href="#"><i class="fa fa-instagram"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- Footer Section End -->
+
+  <!-- Search model Begin -->
+  <div class="search-model">
+    <div class="h-100 d-flex align-items-center justify-content-center">
+      <div class="search-close-switch">+</div>
+      <form class="search-model-form">
+        <input type="text" id="search-input" placeholder="Search here....." />
+      </form>
+    </div>
+  </div>
+  <!-- Search model end -->
+
+  <!-- Js Plugins -->
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/mixitup.min.js"></script>
+  <script src="js/jquery.slicknav.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
